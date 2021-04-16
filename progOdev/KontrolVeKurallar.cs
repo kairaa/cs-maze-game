eca7e5fa190b739ed785895df1285b971dfe47bb
Kayra Çakıroğlu <1030510099@erciyes.edu.tr>
2021-04-16 23:33:43 +0300

    commit message

--- progOdev/KontrolVeKurallar.cs
+++ progOdev/KontrolVeKurallar.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace progOdev
+namespace MazeGame
                             // camelCase standardı kullanılmıştır
 {
     class KontrolVeKurallar
--- progOdev/Program.cs
+++ progOdev/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace progOdev
+namespace MazeGame
                         // camelCase standardı kullanılmıştır
 {
     class Program
